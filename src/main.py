2f857677c2750f300f626aacf71ac01353fac52c
Lucas De Palma <lucasdepalma2002@gmail.com>
2023-04-26 14:43:16 -0300

    Arreglo cagada de lukas

--- src/main.py
+++ src/main.py
@@ -16,27 +16,11 @@ def get_apache(ip_addr, port):
         data = json.load(f)
 
     try:
-<<<<<<< HEAD
-        cnx = http.client.HTTPConnection(ip_addr, port) # Abrimos la conexion
-        cnx.request('GET', '/') # Realiza una solicitud HTTP GET a la raíz del servidor Apache.
-        res = cnx.getresponse() # Obtiene la respuesta del servidor Apache.
-        headers = res.getheaders() # Obtiene los encabezados de la respuesta del servidor Apache.
-        status_cod = res.status # Obtiene el código de estado de la respuesta del servidor Apache.
-=======
-<<<<<<< HEAD
         cnx = http.client.HTTPConnection(ip_addr, port) # Abrimos la conexion
         cnx.request('GET', '/')
         res = cnx.getresponse()
         headers = res.getheaders()
         status_cod = res.status
-=======
-        cnx = http.client.HTTPConnection(ip_addr, port) # Abrimos la conexión
-        cnx.request('GET', '/') # Solicitud HTTP por método GET
-        res = cnx.getresponse() # Guarda la respuesta de esa solicitud
-        headers = res.getheaders() # Guarda los encabezados de la respuesta
-        status_cod = res.status # Guarda el código de estado de la respuesta
->>>>>>> b07408fd52d4c863242cef22647741b4f8f9924f
->>>>>>> master
 
         print('\n+------------------------------+\n')
         print(f"Código de estado: {status_cod}\n")
@@ -44,11 +28,6 @@ def get_apache(ip_addr, port):
         if status_cod == HTTP_OK or HTTP_REDIRECT:
             version_apache = None
             so_apache = None
-<<<<<<< HEAD
-
-            # itera sobre los encabezados de la respuesta y si encuentra el encabezado Server, extrae la versión de Apache y el sistema operativo y los guarda en las variables correspondientes.
-=======
->>>>>>> master
 
             for header in headers:
                 if header[0] == 'Server':
@@ -57,29 +36,13 @@ def get_apache(ip_addr, port):
                     version_apache = server.split('/')[1].split(' ')[0]
                     so_apache = server.split('(')[1].split(')')[0]
                     break
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
-            
->>>>>>> b07408fd52d4c863242cef22647741b4f8f9924f
->>>>>>> master
             for version in data:
                 if version_apache in version:
                     so = data[version][0]
                     version_apache = version.split('-')[0]
                     break
             
-<<<<<<< HEAD
             
-=======
-<<<<<<< HEAD
-            
-=======
->>>>>>> b07408fd52d4c863242cef22647741b4f8f9924f
->>>>>>> master
             else:
                 so = 'Sistema operativo no soportado'
                 version_apache = 'Versión de Apache no soportada'
@@ -95,7 +58,6 @@ def get_apache(ip_addr, port):
     finally:
         cnx.close() # Cerramos la conexión
 
-<<<<<<< HEAD
 
 def verificar_ip(ip_addr):
     try:
@@ -109,32 +71,3 @@ if __name__ == '__main__':
     port = input('Puerto donde está corriendo el Apache: ')
     verificar_ip(ip_addr)
     get_apache(ip_addr, port)
-=======
-<<<<<<< HEAD
-
-def verificar_ip(ip_addr):
-    try:
-        ip_obj = ipaddress.ip_address(ip_addr)
-    except ipaddress.AddressValueError as ValueError:
-        print(f"La IP es inválida: {ValueError}")
-
-
-if __name__ == '__main__':
-    ip_addr = input('Dirección IPv4 o IPv6 del host: ')
-    port = input('Puerto donde está corriendo el Apache: ')
-    verificar_ip(ip_addr)
-    get_apache(ip_addr, port)
-=======
-    def verificar_ip(ip_addr):
-        try:
-            ip_obj = ipaddress.ip_address(ip_addr) # Si crea el objeto correctamente significa que válida la IPv4/v6
-        except ipaddress.AddressValueError as ValueError:
-            print(f"La IP es inválida: {ValueError}")
-
-    if __name__ == '__main__':
-        ip_addr = input('Dirección IPv4 o IPv6 del host: ')
-        port = input('Puerto donde está corriendo el Apache: ')
-        verificar_ip(ip_addr)
-        get_apache(ip_addr, port)
->>>>>>> b07408fd52d4c863242cef22647741b4f8f9924f
->>>>>>> master
